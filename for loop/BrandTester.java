df268ec64a4a88fe2934972649278a95014d9bd6
shushma990 <72930652+shushma990@users.noreply.github.com>
2020-11-04 07:41:05 -0800

    Add files via upload

---
+++ for loop/BrandTester.java
@@ -0,0 +1,8 @@
+public class BrandTester{
+	public static void main(String[] brand){
+		System.out.println("JVM invoked main");
+		CarBrands.getBrands();
+		System.out.println("Exit:: main");
+
+	}
+} 
