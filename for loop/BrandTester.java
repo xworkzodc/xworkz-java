d668b35a997e6749d3bd25511000f10ec09302c4
xworkzodc <48981565+xworkzodc@users.noreply.github.com>
2020-11-05 08:26:11 +0530

    Update BrandTester.java

--- for loop/BrandTester.java
+++ for loop/BrandTester.java
@@ -1,7 +1,13 @@
 public class BrandTester{
 	public static void main(String[] brand){
 		System.out.println("JVM invoked main");
-		CarBrands.getBrands();
+		String[] brands=CarBrands.getBrands();
+		for(int b=0;b<brans.length;b++)
+		{
+		String brand=brands[b];
+		System.out.println("brand"+brand);	
+			
+		}
 		System.out.println("Exit:: main");
 
 	}
